5ff1a2997213d802c179471e3670575bd0a64464
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-01 13:17:28 -0500

    asynch works

--- redux-form-challenge/src/actions/index.js
+++ redux-form-challenge/src/actions/index.js
@@ -1,7 +1,10 @@
 
-const API_BASE_URL = 'https://us-central1-delivery-form-api.cloudfunctions.net/'
+import {SubmissionError} from 'redux-form';
 
-export const report = values => dispatch => {
+const API_BASE_URL = 'https://us-central1-delivery-form-api.cloudfunctions.net/';
+
+export const reportIssue = values => dispatch => {
+  console.log(JSON.stringify(values));
   return fetch(`${API_BASE_URL}api/report`,{
     method: 'POST',
     body: JSON.stringify(values),
@@ -9,31 +12,29 @@ export const report = values => dispatch => {
       'Content-Type': 'application/json'
     }
   })
-  .then(res => {
-    if(!res.ok){
-      if(
-      res.headers.has('content-type')&&
+    .then(res => {
+      if(!res.ok){
+        if(
+          res.headers.has('content-type')&&
       res.headers
         .get('content-type')
         .startsWith('application/json')
-      ){
+        ){
           return res.json().then(err => Promise.reject(err));
+        }
+        return Promise.reject({
+          code: res.status,
+          message: res.statusText
+        });
       }
-      return Promise.reject({
-        code: res.status,
-        message: res.statusText
-      });
-    }
-    return;
-  })
-  .then(() => console.log('Submitted report', report))
-  .catch(error =>
-    Promise.reject(
-      new SubmissionError({
-        [error.location]: error.message
-      })
-    )  
-  )
-  
-
-}
+      return;
+    })
+    .then(() => console.log('Submitted report', values))
+    .catch(error =>
+      Promise.reject(
+        new SubmissionError({
+          [error.location]: error.message
+        })
+      )  
+    );
+};
--- redux-form-challenge/src/components/complaintForm.js
+++ redux-form-challenge/src/components/complaintForm.js
@@ -3,37 +3,37 @@ import './complaintForm.css';
 import{reduxForm, Field} from 'redux-form';
 import{required, notEmpty, lengthOfFive, onlyNumbers} from '../validators/validators';
 import TrackingNumber from './trackingNumber';
+import {reportIssue} from '../actions/index';
 
 class ComplaintForm extends React.Component {
   render() {
+    if(this.props.submitSucceeded) {
+      return <div>'We have received your report'</div>;
+    }
     return (
       <div className="delivery-form">
         <h2>Report a problem with your deliver</h2>
-        <form>
-          <div>
-            <label htmlFor="trackingNumber">Tracking Number</label>
-            <Field 
-              component={TrackingNumber}
-              element='input' 
-              name="trackingNumber" 
-              value 
-              id="trackingNumber"
-              validate={[required, notEmpty, lengthOfFive, onlyNumbers]} />
-          </div>
-          <div>
-            <label htmlFor="issue">What is your issue?</label>
-            <Field component='select' name="issue" id="issue">
-              <option value="not-delivered">My delivery hasn't arrived.</option>
-              <option value="wrong-item">Wrong item delivered.</option>
-              <option value="missing-item">Part of my order was missing.</option>
-              <option value="damaged-item">Some of my order arrived damaged.</option>
-              <option value="other">Other (give details below).</option>
-            </Field>
-          </div>
-          <div>
-            <label htmlFor="details">Give more details (optional)</label>
-            <Field component='textarea' name="details" id="details" />
-          </div>
+        <form onSubmit={this.props.handleSubmit(values => 
+          this.props.dispatch(reportIssue(values))
+        )}>
+          <label htmlFor="trackingNumber">Tracking Number</label>
+          <Field 
+            component={TrackingNumber}
+            element='input' 
+            name="trackingNumber" 
+            value 
+            id="trackingNumber"
+            validate={[required, notEmpty, lengthOfFive, onlyNumbers]} />
+          <label htmlFor="issue">What is your issue?</label>
+          <Field component='select' name="issue" id="issue">
+            <option value="not-delivered">My delivery hasn't arrived.</option>
+            <option value="wrong-item">Wrong item delivered.</option>
+            <option value="missing-part">Part of my order was missing.</option>
+            <option value="damaged">Some of my order arrived damaged.</option>
+            <option value="other">Other (give details below).</option>
+          </Field>
+          <label htmlFor="details">Give more details (optional)</label>
+          <Field component='textarea' value name="details" id="details" />
           <button tyle="submit">Submit</button>
         </form>
       </div>
